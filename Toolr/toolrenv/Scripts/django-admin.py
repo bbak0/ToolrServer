f5ddcdb23f3cd1191321eabedb5a04aceea1beff
bbak0 <b_bak@outlook.com>
2018-03-13 11:31:57 +0100

    initial commit

---
+++ Toolr/toolrenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\zordon\docume~1\nowyfo~1\toolr\toolr\toolrenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ ToolrAPI/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ToolrapiConfig(AppConfig):
+    name = 'ToolrAPI'
